01f9b6a2810df72e59c1d920f787c6cff34c55ce
Vaibhav <vaibhavjam86@gmail.com>
2025-12-04 11:35:13 +0530

    ec2 terraform update

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,8 +1,3 @@
-resource "aws_key_pair" "deployer" {
-  key_name   = "terra-automate-key"
-  public_key = file("/Users/shubham/Documents/work/TrainWithShubham/terra-practice/terra-key.pub")
-}
-
 resource "aws_default_vpc" "default" {
 
 }
@@ -11,19 +6,12 @@ resource "aws_security_group" "allow_user_to_connect" {
   name        = "allow TLS"
   description = "Allow user to connect"
   vpc_id      = aws_default_vpc.default.id
+
   ingress {
     description = "port 22 allow"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = " allow all outgoing traffic "
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -43,6 +31,14 @@ resource "aws_security_group" "allow_user_to_connect" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    description = "allow all outgoing traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "mysecurity"
   }
@@ -51,13 +47,18 @@ resource "aws_security_group" "allow_user_to_connect" {
 resource "aws_instance" "testinstance" {
   ami             = var.ami_id
   instance_type   = var.instance_type
-  key_name        = aws_key_pair.deployer.key_name
+
+  # Use your existing key pair
+  key_name        = "my-aws-key"
+
   security_groups = [aws_security_group.allow_user_to_connect.name]
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
   tags = {
     Name = "Automate"
-  }
-  root_block_device {
-    volume_size = 30 
-    volume_type = "gp3"
   }
 }
