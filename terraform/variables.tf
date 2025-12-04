1573f9c3a2d994dc405e6b04a3efcc601d34293c
Vaibhav <vaibhavjam86@gmail.com>
2025-12-04 12:48:55 +0530

    ami id changed

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
-  default     = "ami-085f9c64a9b75eed5"
+  default     = "ami-0f5fcdfbd140e4ab7"
 }
 
 variable "instance_type" {
